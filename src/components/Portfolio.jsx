d01c6f8f3280a7017f241c5ac1ad543157fd4fc5
Your Name <your_email@example.com>
2023-08-22 23:52:25 +0530

    I have added url for project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,18 +8,25 @@ const Portfolio = () => {
         {
             id: 1,
             src: food,
-            title: 'Online food ordering platform'
+            title: 'Online food ordering platform',
+            demourl: 'https://online-food-ordering-app-by-snehal.netlify.app/',
+            codeurl: 'https://github.com/snehal0527/react-course'
         },
         {
             id: 2,
             src: landing,
-            title: 'Responsive landig page'
+            title: 'Responsive landing page',
+            demourl: '',
+            codeurl: ''
         },{
             id: 3,
             src: todo,
-            title: 'To do list application'
+            title: 'To do list application',
+            demourl: '',
+            codeurl: ''
         }
     ]
+    
   return (
     <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 text-white w-full md:h-screen'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
@@ -30,13 +37,23 @@ const Portfolio = () => {
 
             <div className='grid sm:grid-cols-1 md:grid-cols-3 gap-8 py-4 px-12 sm:px-0'>
                 {
-                    portfolios.map(({id, src, title}) => (
+                    portfolios.map(({id, src, title, demourl, codeurl }) => (
                         <div className='shadow-md shadow-gray-600 rounded-lg'>
                             <img key={id} src={src} alt='' className='rounded-md duration-200 hover:scale-105'></img>
+                            <p className='flex items-center justify-center mt-2'>{title}</p>
                             <div className='flex items-center justify-center'>
-                                {/* <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button> */}
-                                <p>{title}</p>
+                                <button className='w-1/2 px-6 ml-4- mr-4 mb-2 mt-2 duration-200 hover:scale-105' onClick={() => {
+                                    if(demourl) {
+                                        window.open(demourl, '_blank')
+                                    }
+                                }
+                                }>Demo</button>
+                                <button className='w-1/2 px-6 ml-4- mr-4 mb-2 mt-2 duration-200 hover:scale-105' onClick={() => {
+                                    if(codeurl) {
+                                        window.open(codeurl, '_blank')
+                                    }
+                                }
+                                }>Code</button> 
                             </div>
                         </div>
                     ))
